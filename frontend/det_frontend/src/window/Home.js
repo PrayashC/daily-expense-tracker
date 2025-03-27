dfce2d68ec1ebcee6d4f18e1675e118a2c7fa199
PrayashC <you@example.com>
2025-03-27 17:31:06 +0530

    added express as backend

--- frontend/det_frontend/src/window/Home.js
+++ frontend/det_frontend/src/window/Home.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(async () => {
+        try{
+            const response = await axios.get("http://localhost:5000");
+            setData(response.data);
+        } catch(e) {
+            console.log(e);
+        }
+    }, []);
+
     return (
         <div>
             <h1>This is home page.</h1>
+            <p>{data ? data : "loading..."}</p>
         </div>
     )
 }
