0c5ac08ff77f86c34a091d68417d1b58220eb869
PrayashC <you@example.com>
2025-03-28 13:42:14 +0530

    error fixed

--- frontend/det_frontend/src/window/Home.js
+++ frontend/det_frontend/src/window/Home.js
@@ -4,13 +4,16 @@ import axios from "axios";
 const Home = () => {
     const [data, setData] = useState(null);
 
-    useEffect(async () => {
-        try{
-            const response = await axios.get("https://daily-expense-tracker-rmmm.onrender.com/");
-            setData(response.data);
-        } catch(e) {
-            console.log(e);
+    useEffect(() => {
+        const getData = async () => {
+            try{
+                const response = await axios.get("https://daily-expense-tracker-rmmm.onrender.com/");
+                setData(response.data);
+            } catch(e) {
+                console.log(e);
+            }
         }
+        getData();
     }, []);
 
     return (
