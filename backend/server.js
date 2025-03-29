a762b0a7cec38d8ddef638758a9d996a64ac64a9
PrayashC <you@example.com>
2025-03-29 12:22:16 +0530

    run server.js in local

--- backend/server.js
+++ backend/server.js
@@ -7,30 +7,37 @@ dotenv.config();
 const app = express();
 app.use(json());
 app.use(cors());
+mongoose.set('debug', true);
 
-mongoose.connect(process.env.ATLAS_URI)
+connect(process.env.ATLAS_URI)
 .then(() => console.log("MongoDB connected!"))
 .catch(err => console.log(err));
 
+const db = mongoose.connection;
+db.on('connected', () => console.log('connected'));
+db.on('open', () => console.log('open'));
+db.on('disconnected', () => console.log('disconnected'));
+
 const UserSchema = new mongoose.Schema({
-    username:  { type: String },
-    password:  { type: String },
+    username:  String,
+    password:  String,
 });
 
 const User = mongoose.model("User", UserSchema, "users");
+User.find({}).lean().then(users => console.log(users)).catch(err => console.error(err));
 
-app.get("/", (req, res) => {
-    const getUserData = async () => {
-        try {
-            const users = await User.find();
-            res.json(users);
-            console.log(users);
-        } catch (e) {
-            res.status(500).json({error: "Failed to fetch data"});
-        }
+
+app.get("/", async (req, res) => {
+    try {
+        const users = await User.find({}).lean();
+        res.json(users);
+        console.log(users);
+    } catch (e) {
+        res.status(500).json({ error: "Failed to fetch data" });
+        console.error(e);
     }
-    getUserData();
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
