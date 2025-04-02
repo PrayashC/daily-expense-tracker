5daf84fef491ab6c389ab91a0bbd76a9cece163a
Dhruvtxt <dhruv808080@gmail.com>
2025-04-03 02:59:49 +0530

    5000 to 6000

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
 import app from './expressApp.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
