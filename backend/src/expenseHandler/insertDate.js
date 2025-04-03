f017a480fa79c0237ddbd034ecf05dd458a3a936
Dhruvtxt <dhruv808080@gmail.com>
2025-04-03 08:49:07 +0530

    date exist, error message ,$push position so it iterate without checking every folder

--- backend/src/expenseHandler/insertDate.js
+++ backend/src/expenseHandler/insertDate.js
@@ -5,9 +5,16 @@ const InsDate = async (req, res) => {
     try{
         const dateExist = await expensesCollection.findOne({ _id: userId, "expenseList.date": date });
 
-        if (dateExist) {
+        if (dateExist) 
+            {
+                // 1.if the entered date already exists.
+                return res.status(400).json({ message: "This Date exists" });
+            } 
+             
             await expensesCollection.updateOne(
-                { 
+                //adding the data but checking the id first or it updates eeverything first
+                { _id: userId }, 
+                {
                     $push: { 
                         expenseList: { 
                             date: date,
@@ -15,16 +22,15 @@ const InsDate = async (req, res) => {
                                 {
                                   expend: String,
                                   cost: Number
-                                }
-                            ] 
-                        } 
+                                }] } 
                     } 
                 },
-                { upsert: true })
+                { upsert: true }
+            );
             return res.status(200).json({ message: "Date added successfully!" });
-        }
+
     } catch (error) {
-        res.status(500).json({ message: "Error inserting date" });
+        res.status(500).json({ message: "Error inserting date",error: error });
     }
 }
 
